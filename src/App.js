a852a2e0055f656fa271989d5e77dbe8ff697981
Sanjay yadav <Sanjayadav448@gmail.com>
2024-01-18 19:02:04 +0530

    day2 bg color changer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+
+ const [color,setColor] =useState("chocolate")
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="" style={{backgroundColor:color,height:"100vh"}}>
+    <div className='fixed-bottom d-flex justify-content-center  p-2 mb-4 rounded'>
+      <div className='p-2 px-4 rounded-pill' style={{backgroundColor:"white"}}>
+      <button className='bg-red rounded btn btn-danger ms-2 gap-3' onClick={()=> setColor("Red")} >Red</button>
+      <button className='bg-red rounded btn text-white ms-2 gap-3' onClick={()=>setColor("Yellow")} style={{backgroundColor:"Yellow"}}  >Yellow</button>
+      <button className='bg-red rounded btn text-white ms-2 gap-3' onClick={()=>setColor("Purple")} style={{backgroundColor:"Purple"}}>Purple</button>
+      <button className='bg-red rounded btn text-white ms-2 gap-3' onClick={()=>setColor("Green")} style={{backgroundColor:"Green"}}>Green</button>
+      <button className='bg-red rounded btn text-white ms-2 gap-3' onClick={()=>setColor("Pink")} style={{backgroundColor:"Pink"}}>Pink</button>
+        
+        </div>
+        </div>
+        </div>
   );
 }
 
